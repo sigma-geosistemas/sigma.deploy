4958c6063a9e328eda2bd0371fb8123a26d73467
George Silva <george@sigmageosistemas.com.br>
2016-07-19 15:39:49 -0300

    arrumando essa zica

--- deploy/opbeat.py
+++ deploy/opbeat.py
@@ -1,4 +1,5 @@
-from fabric.api import runs_once, lcd, local, task
+from fabric.api import runs_once, local, task
+from fabric.context_managers import cd
 from fabric.state import env
 
 
@@ -12,7 +13,7 @@ def register_deployment(path, org, app, secret):
                                app=<app>,
                                secret=<secret>
     """
-    with(lcd(path)):
+    with(cd(path)):
         revision = local('git log -n 1 --format:oneline', capture=True).split(" ")[0]
         branch = local('git rev-parse --abbrev-ref HEAD', capture=True)
         local('curl https://intake.opbeat.com/api/v1/organizations/{}/apps/{}/releases/'
